2987a791004760cf0983d37363411cf823846085
ahhxfeng <ahhxfeng@gmail.com>
2018-01-04 09:27:33 +0800

    备份

---
+++ hxf_new1/hxf_new/client/client.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"time"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"math/rand"
+	// "sort"
+	// "net/url"
+)
+const PhoneType = "ABC"
+const name = "abcdefghijklmnopqrstuvwxyz"
+const phone = "0123456789"
+func  RandomString(pixff string, strlen int) string {
+	rand.Seed(time.Now().UnixNano())
+	result := make([]byte, strlen)
+	for i := 0; i < strlen; i++ {
+		result[i] = pixff[rand.Intn(len(pixff))]
+	}
+	return string(result)
+}
+
+
+
+
+
+// type Items struct {
+//     // Ono string `json:ono`
+//     OrderItem []Item `json:item`
+//     // OrderRefund []Refund `json:refund`
+// }
+
+type ItemA struct {
+    Name string `json:name`
+    PhoneNumber string    `json:phone_number`
+}
+
+type ItemB struct{
+	NickName string `json:name`
+	Phone string `json:phone`
+	Address string `json:address`
+}
+
+type ItemC struct{
+	FullName string `json:name`
+	Telephone string `json:phone`
+	Note string `json:note`
+}
+type Items struct{
+	ItemA []ItemA
+	ItemB []ItemB
+	ItemC []ItemC
+}
+type ItemsA struct{
+	ItemA []ItemA
+}
+
+
+type PhoneSorter interface {
+	getName()
+	// sortByName()
+}
+
+
+func main() {
+
+
+	var m Items
+	phonetype := RandomString(PhoneType, 1)
+	for i := 0; i < 4; i++ {
+		switch phonetype {
+		case "A":
+			m.ItemA = append(m.ItemA, ItemA{Name: RandomString(name, 1), PhoneNumber: RandomString(phone, 4)})
+		case "B":
+			m.ItemB = append(m.ItemB, ItemB{NickName: RandomString(name, 1), Phone: RandomString(phone, 4), Address: RandomString(name, 5)})
+		case "C":
+			m.ItemC = append(m.ItemC, ItemC{FullName: RandomString(name, 1), Telephone: RandomString(phone, 4), Note: RandomString(name, 5)})
+		}
+		time.Sleep(1000)
+	}
+	M := make(map[string][]struct{})
+	M["items"] = m
+	// M := {
+	// 	"items": [
+	// 		{
+	// 			"name": "c",
+	// 			"phone_number": "1811"
+	// 		},
+	// 		{
+	// 			"name": "b",
+	// 			"phone_number": "1161"
+	// 		},
+	// 		{
+	// 			"name": "a",
+	// 			"phone_number": "9111"
+	// 		}
+	// 	]
+	// }
+	
+	fmt.Println(M)
+	c, err := json.Marshal(m)
+	if err != nil {
+		fmt.Println("json err:", err)
+	}
+
+	body := bytes.NewBuffer([]byte(c))
+	res, err := http.Post("http://localhost:9001/sort?type="+ phonetype, "application/json;charset=utf-8", body)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	result, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Printf("%s", result)
+}
---
+++ hxf_new1/hxf_new/server.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html"
+	"io/ioutil"
+	"net/http"
+	// "strings"
+	"sort"
+	"bytes"
+)
+
+
+
+type ItemA struct {
+    Name string `json:name`
+    PhoneNumber string    `json:phone_number`
+}
+
+type ItemB struct{
+	NickName string `json:name`
+	Phone string `json:phone`
+	Address string `json:address`
+}
+
+type ItemC struct{
+	FullName string `json:name`
+	Telephone string `json:phone`
+	Note string `json:note`
+}
+
+type PhoneGetNamer interface {
+	getName() string
+	// sortByName() struct
+}
+// type PhoneSorter interface{
+// 	PhoneGetNamer
+// 	sortByName() struct
+// }
+
+// func (i Items)sortByName() struct{
+// 	var i[0] PhoneGetNamer = new(type)
+// }
+
+type Items struct{
+	ItemA []ItemA
+	ItemB []ItemB
+	ItemC []ItemC
+}
+type ItemsA struct{
+	ItemA []ItemA
+}
+// type Items struct {
+//     // Ono string `json:ono`
+//     OrderItem []Item `json:item`
+//     // OrderRefund []Refund `json:refund`
+// }
+// type Item struct {
+//     Name string `json:name`
+//     Phone_number string    `json:phone_number`
+// }
+func (i *ItemA)getName() string{
+	name := i.Name
+	return name
+}
+func (i *ItemB)getName() string{
+	name := i.NickName
+	return name
+}
+func (i *ItemC)getName() string{
+	name := i.FullName
+	return name
+}
+
+// func (i ItemA)sortByName() struct {
+// 	sort.Sort(sortLista(i))
+// 	return i
+// }
+// func (i *ItemsA)sortByName()  struct{
+// 	sort.Sort(sortList(ItemsA.ItemA))
+// 	return ItemsA
+// }
+// type nameList []ItemA
+// type sortList []PhoneGetNamer
+// func (a sortList) Len() int {return len(a)}
+// func (a sortList) Swap(i, j int) {a[i], a[j] = a[j], a[i]}
+// func (a sortList) Less(i, j int) bool {
+// 	return a[i].getName() < a[j].getName()
+// }
+type sortLista []ItemA
+func (a sortLista) Len() int {return len(a)}
+func (a sortLista) Swap(i, j int) {a[i], a[j] = a[j], a[i]}
+func (a sortLista) Less(i, j int) bool {
+	return a[i].getName() < a[j].getName()
+}
+
+// type sortListb []ItemB
+// func (a sortListb) Len() int {return len(a)}
+// func (a sortListb) Swap(i, j int) {a[i], a[j] = a[j], a[i]}
+// func (a sortListb) Less(i, j int) bool {
+// 	return a[i].getName() < a[j].getName()
+// }
+
+// type sortListc []ItemC
+// func (a sortListc) Len() int {return len(a)}
+// func (a sortListc) Swap(i, j int) {a[i], a[j] = a[j], a[i]}
+// func (a sortListc) Less(i, j int) bool {
+// 	return a[i].getName() < a[j].getName()
+// }
+// type sortByName []Item
+// func (a sortByName) Len() int { return len(a)}
+// func (a sortByName) Swap(i, j int) {a[i], a[j] = a[j], a[i]}
+// func (a sortByName) Less(i, j int) bool {
+// 	if a[i].Name < a[j].Name {
+// 		return 	true
+// 	}
+// 	if a[i].Name > a[j].Name {
+// 		return 	false
+// 	}
+// 	return a[i].Name < a[j].Name
+// }
+
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":9001", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm() //解析参数，默认是不会解析的
+	fmt.Fprintf(w, "Hi, wait for sort %s", html.EscapeString(r.URL.Path[1:]))
+	if r.Method == "GET" {
+		fmt.Println("method:", r.Method) //获取请求的方法
+	} else if r.Method == "POST" {
+		result, _ := ioutil.ReadAll(r.Body)
+		r.Body.Close()
+		fmt.Printf("%s\n", result)
+		phoneType := r.FormValue("type")
+		fmt.Printf("%+v", phoneType)
+		var items Items
+		decoder := json.NewDecoder(bytes.NewBuffer(result))
+		err := decoder.Decode(&items)
+		if err != nil {
+			panic(err)
+		}
+		sort.Sort(sortLista(items.ItemA))
+		json.NewEncoder(w).Encode(items)
+		if err := json.Unmarshal(result, &items); err == nil{
+			fmt.Printf("%+v", items)
+			// fmt.Fprint(w, items)
+			// fmt.Printf(phoneData.TYPE)
+			// resp,_ := json.Marshal(phoneData)
+
+		}
+	}
+}
